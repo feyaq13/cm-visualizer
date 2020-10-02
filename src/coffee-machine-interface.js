7d5e3bc8d6bb1ba68c677d0726e0cc88203804cf
Semyon Fomin <taxnuke@gmail.com>
2020-10-02 18:16:31 +0300

    ревьюит весь js-код

--- src/coffee-machine-interface.js
+++ src/coffee-machine-interface.js
@@ -1,69 +1,80 @@
-import audioURL from './assets/sounds/switch-click-button.mp3'
+import audioURL from './assets/sounds/switch-click-button.mp3';
 
 export class CoffeeMachineInterface {
-  constructor(imageSource) {
+  constructor() {
     this._soundClickButtons = new Audio(audioURL);
     this._buttonElements = document.getElementsByClassName('button');
-    this._switchOnButton =  Array.prototype.filter.call(this._buttonElements, (button => button.classList.contains('button_is-switch-on')))[0]
+    this._switchOnButton = Array.prototype.filter.call(this._buttonElements, (button) =>
+      button.classList.contains('button_is-switch-on'),
+    )[0];
     this._buttonElementsNav = document.getElementsByClassName('coffee-list')[0];
     this._cupElement = document.getElementsByClassName('coffee-cup')[0];
   }
 
   setupOnMakeCoffeeTypesOnEventClick() {
-    return new Promise((resolve => {
-      this._buttonElementsNav.addEventListener('click', this._getTypeCoffee.bind(this, typeOfCoffee => resolve(typeOfCoffee)))
-    }))
+    return new Promise((resolve) => {
+      this._buttonElementsNav.addEventListener(
+        'click',
+        this._getTypeCoffee.bind(this, (coffeeType) => resolve(coffeeType)),
+      );
+    });
   }
 
   _getTypeCoffee(cb, e) {
     if (e.target.type === 'button') {
-      this.onPendingAnimation()
-      this.disabledAllButtons(e)
-      return cb(e.target.textContent)
+      this.onPendingAnimation();
+      this.disableAllButtons(e);
+
+      return cb(e.target.textContent);
     }
   }
 
-  disabledAllButtons(e) {
-    Array.prototype.forEach.call(e.currentTarget.getElementsByTagName('button'), (button) => button.disabled = true)
+  disableAllButtons(e) {
+    Array.prototype.forEach.call(e.currentTarget.getElementsByTagName('button'), (button) => (button.disabled = true));
   }
 
-  enabledAllButtons() {
-    Array.prototype.forEach.call(this._buttonElementsNav.getElementsByTagName('button'), (button) => button.disabled = false)
+  enableAllButtons() {
+    Array.prototype.forEach.call(
+      this._buttonElementsNav.getElementsByTagName('button'),
+      (button) => (button.disabled = false),
+    );
   }
 
   setupOnCleanWasteOnEventClick(cb) {
-    document.getElementsByClassName('button-clean-waste')[0].addEventListener('click', cb)
+    document.getElementsByClassName('button-clean-waste')[0].addEventListener('click', cb);
   }
 
   async _playSoundClickButtons() {
     try {
-      await this._soundClickButtons.play()
+      await this._soundClickButtons.play();
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   }
 
   onPendingAnimation() {
-    this._switchOnButton.classList.add('pending-mode')
+    this._switchOnButton.classList.add('pending-mode');
   }
 
   onPouringDrinkAnimation(ms, colorCoffee) {
     this._cupElement.style.fill = colorCoffee;
-    this._cupElement.classList.add('pouring-mode')
+    this._cupElement.classList.add('pouring-mode');
     this._cupElement.style.animationDuration = ms;
   }
 
   stopPendingAnimation() {
-    this._switchOnButton.setAttribute('aria-checked', "true")
-    this._switchOnButton.classList.remove('pending-mode')
+    this._switchOnButton.setAttribute('aria-checked', 'true');
+    this._switchOnButton.classList.remove('pending-mode');
   }
 
   setupPlaySoundOnEventClick() {
-    Array.prototype.forEach.call(this._buttonElements, (button => button.addEventListener('click', this._playSoundClickButtons.bind(this))))
+    Array.prototype.forEach.call(this._buttonElements, (button) =>
+      button.addEventListener('click', this._playSoundClickButtons.bind(this)),
+    );
   }
 
   setupOnSwitchOnEventClick(cb) {
-    this._switchOnButton.addEventListener('click', cb, {once: true})
+    this._switchOnButton.addEventListener('click', cb, { once: true });
   }
 
   showTypesCoffee(coffee) {
@@ -71,13 +82,13 @@ export class CoffeeMachineInterface {
       const coffeeListElement = document.getElementsByClassName('coffee-list')[0];
 
       coffee.forEach((coffeeName) => {
-        const buttonElement = document.createElement('button')
-        buttonElement.type = 'button'
-        const listItemElement = document.createElement('li')
-        buttonElement.textContent = coffeeName
+        const buttonElement = document.createElement('button');
+        buttonElement.type = 'button';
+        const listItemElement = document.createElement('li');
+        buttonElement.textContent = coffeeName;
         listItemElement.appendChild(buttonElement);
-        coffeeListElement.appendChild(listItemElement)
-      })
+        coffeeListElement.appendChild(listItemElement);
+      });
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import { CoffeeMachineInterface } from './coffee-machine-interface'
+import { CoffeeMachineInterface } from './coffee-machine-interface';
 import { CoffeeMachine } from './coffee-machine';
 
-const myCoffeeMachine = new CoffeeMachine(new CoffeeMachineInterface(), true, false)
-// myCoffeeMachine.makeCoffee('cappuccino', 'ground')
+const myCoffeeMachine = new CoffeeMachine(new CoffeeMachineInterface(), true);
 
-window.cm = myCoffeeMachine
+window.cm = myCoffeeMachine;
